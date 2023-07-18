f153384f9c166efc0a029d4023b7459b82b57b37
Esi're <esiri92@gmail.com>
2023-07-18 18:57:50 +0000

    chore: update index.html & index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,290 +1,74 @@
 import { Account, Call } from '@core/types/phone';
+import { Log, CallInfo, GlobalEventNames } from '@core/types/events';
+
+type Events = GlobalEventNames | 'widgetReady';
+
+const log = (...args: any[]) => console.log('widget-client', ...args);
 
 // @ts-ignore
 const client = ZAFClient.init();
 
-// client.invoke('resize', { width: '400px', height: '520px' });
-
-interface VoiceDialoutEvent {
-  number: string;
-}
-
-client.on('app.activated', function (data: any) {
-  console.log('app.activated', { data });
-});
+// client.on('app.activated', (data: any) => console.log('app.activated', { data }));
 
 client.on('app.registered', (data: any) => {
   // console.log('app.registered', { data });
 
-  // window.Brekeke.widget.events(events => {
-  //
-  // });
+  let host: string, port: string, url: string;
 
-  window.Brekeke.renderWidget(
-    document.getElementById('widget_embed_div')!,
-    ({
-       fireCallInfoEvent,
-       fireConfigEvent,
-       fireLogSavedEvent,
-       fireMakeCallEvent,
-       onCallUpdatedEvent,
-       onCallEndedEvent,
-       onLoggedOutEvent,
-       onLoggedInEvent,
-       onCallEvent,
-       onLogEvent,
-     }) => {
-      const autoTicket = false;
+  const form = document.getElementById('widget-placeholder')! as HTMLFormElement;
+  const hostEl = form.elements.namedItem('host')! as HTMLInputElement;
+  const portEl = form.elements.namedItem('port')! as HTMLInputElement;
 
-      fireConfigEvent({
-        enableLog: !autoTicket,
-        enableLogDescription: true,
-        enableLogResult: false,
-        logButtonTitle: 'Create Ticket',
-      });
+  hostEl.addEventListener('change', () => host = hostEl.value);
+  portEl.addEventListener('change', () => port = portEl.value);
 
-      let dialOut: VoiceDialoutEvent | undefined;
-      let currentCall: Call | undefined;
-      let agent: any | undefined;
-      let pbxAccount: Account | undefined;
+  form.addEventListener('submit', e => {
+    e.preventDefault();
 
-      let recentTicketId: number | undefined;
+    if (!host || !port) return;
 
-      // add click-to-call listener
-      // @ts-ignore
-      client.on('voice.dialout', e => {
-        console.log('voice.dialout', e);
-        dialOut = e;
-        fireMakeCallEvent(e.number);
-      });
+    url = `https://${host}:${port}/pbx/etc/widget/zendesk/index.html`;
 
-      onLoggedInEvent(account => {
-        pbxAccount = account;
-        client.request('/api/v2/users/me.json')
-          .then((data: any) => {
-            console.log('me', { data });
-            if (data) {
-              agent = data.user;
-            }
-          })
-          .catch((error: any) => {
-            console.error(error.toString());
-          });
-      });
+    (document.getElementById('widget-container') as HTMLIFrameElement).src = url;
+  });
 
-      onLoggedOutEvent(() => {
-        currentCall = undefined;
-        dialOut = undefined;
-        agent = undefined;
-        pbxAccount = undefined;
-      });
+  let widgetWindow: Window;
 
-      onCallUpdatedEvent(call => void (currentCall = call));
-      onCallEndedEvent(call => {
-        if (call.id === currentCall?.id) {
-          currentCall = undefined;
-        }
+  const messageName = (name: Events) => `brekeke:${name}`;
 
-        if (dialOut?.number === call.partyNumber) {
-          dialOut = undefined;
-        }
-      });
+  const sendMessage = <T>(name: Events, data?: T) => {
+    if (!widgetWindow) return;
+    try {
+      widgetWindow.postMessage(JSON.stringify({ name: messageName(name), data }), '*');
+    } catch (e) {
+      log('send message error', name, e);
+    }
+  };
 
-      onCallEvent(call => {
-        console.log('onCallEvent', call);
+  window.addEventListener('message', ev => {
+    try {
+      const { name, data } = JSON.parse(ev.data);
+      if (!name || (typeof name == 'string' && !name.startsWith('brekeke:'))) return;
 
-        // dock the panel
-        client.invoke('popover', 'show');
+      log(`${name} message received`, ev);
+      log(`${name} message data`, data);
 
-        const phone = call.partyNumber;
-        let query = `/api/v2/search.json?query=role:end-user phone:${phone}`;
-        // query = encodeURIComponent(query);
+      switch (name) {
+        case messageName('widgetReady'):
+          log('widget ready');
+          widgetWindow = ev.source as Window;
+          document.getElementById('widget-placeholder')!.remove();
+          client.invoke('resize', { width: '400px', height: '520px' });
+          break;
+      }
+    } catch (e) {
+      log('message error, invalid json string', e);
+    }
+  });
 
-        client.request(query)
-          .then((data: any) => {
-            console.log('search', { data });
-
-            if (data.results.length > 0) {
-              const customer = data.results[0];
-
-              fireCallInfoEvent(call, {
-                id: customer.id,
-                name: customer.name,
-              });
-
-              if (!autoTicket) {
-                client.invoke('routeTo', 'user', customer.id);
-              } else {
-                client.request(`/api/v2/users/${customer.id}/tickets/requested.json?sortby=created_at&sort_order=desc`)
-                  .then((data: any) => {
-                    console.log('tickets', { data });
-
-                    if (data.tickets.length > 0) {
-                      recentTicketId = data.tickets[0].id;
-                      client.invoke('routeTo', 'ticket', recentTicketId);
-                    } else {
-                      createTicket(call, customer);
-                    }
-                  })
-                  .catch((error: any) => {
-                    console.error('find ticket error', error.toString());
-                  });
-              }
-            } else {
-              const newCustomer = {
-                user: {
-                  name: `Caller ${phone}`,
-                  phone,
-                },
-              };
-
-              const config = {
-                url: '/api/v2/users.json',
-                type: 'POST',
-                cache: false,
-                contentType: 'application/json',
-                httpCompleteResponse: true,
-                data: JSON.stringify(newCustomer),
-              };
-
-              client.request(config)
-                .then((data: any) => {
-                  console.log('create user', { data });
-
-                  const result = JSON.parse(data.responseText);
-                  console.log('create user', { result });
-                  if (result.user.id) {
-                    const customer = result.user;
-
-                    fireCallInfoEvent(call, {
-                      id: customer.id,
-                      name: customer.name,
-                    });
-
-                    if (autoTicket) {
-                      createTicket(call, customer);
-                    } else {
-                      client.invoke('routeTo', 'user', customer.id);
-                    }
-                  }
-                })
-                .catch((err: any) => {
-                  console.error('create user error', err);
-                });
-            }
-          })
-          .catch((err: any) => {
-            console.error('search error', err);
-          });
-      });
-
-      onLogEvent(log => {
-        console.log('logEvent', log);
-        const call = log.call;
-
-        const voice_comment = {
-          answered_by_id: agent.id,
-          call_duration: Math.trunc(log.duration / 1000),
-          from: call.incoming ? call.partyNumber : log.user,
-          to: call.incoming ? log.user : call.partyNumber,
-          recording_url: 'https://samplelib.com/lib/preview/mp3/sample-6s.mp3',
-          started_at: new Date(call.answeredAt),
-          // location: 'Dublin, Ireland',
-          // transcription_text: 'The transcription of the call',
-        };
-
-        const ticketData = {
-          display_to_agent: agent.id,
-          ticket: {
-            via_id: call.incoming ? 45 : 46,
-            subject: log.subject,
-            description: log.description,
-            comment: { body: log.comment },
-            requester_id: log.recordId,
-            assignee_id: agent.id,
-            // voice_comment, // body comment seems to override this, so I'll PUT this after creating ticket with body comment.
-          },
-        };
-
-        const config = {
-          url: '/api/v2/channels/voice/tickets.json',
-          type: 'POST',
-          cache: false,
-          contentType: 'application/json',
-          httpCompleteResponse: true,
-          data: JSON.stringify(ticketData),
-        };
-
-        client.request(config)
-          .then((data: any) => {
-            let result = JSON.parse(data.responseText);
-            if (result.ticket) {
-              fireLogSavedEvent(log);
-              const id = result.ticket.id;
-              console.log('ticket created' + result.ticket);
-              client.invoke('routeTo', 'ticket', recentTicketId);
-
-              const voiceData = { ticket: { voice_comment } };
-
-              const config = {
-                url: `/api/v2/tickets/${id}.json`,
-                type: 'PUT',
-                cache: false,
-                contentType: 'application/json',
-                httpCompleteResponse: true,
-                data: JSON.stringify(voiceData),
-              };
-
-              client.request(config)
-                .then((data: any) => {
-                  console.log('recording updated', data);
-                })
-                .catch((error: any) => {
-                  console.error('recording update error', error);
-                });
-            }
-          })
-          .catch((error: any) => {
-            console.error('ticket error', error);
-          });
-      });
-
-      const createTicket = (call: Call, customer: any) => {
-        const ticketData = {
-          display_to_agent: agent.id,
-          ticket: {
-            via_id: call.incoming ? 45 : 46,
-            subject: 'New Ticket',
-            requester_id: customer.id,
-            description: `Call with ${call.partyNumber}`,
-            assignee_id: agent.id,
-          },
-        };
-
-        const config = {
-          url: '/api/v2/channels/voice/tickets.json',
-          type: 'POST',
-          cache: false,
-          contentType: 'application/json',
-          httpCompleteResponse: true,
-          data: JSON.stringify(ticketData),
-        };
-
-        client.request(config)
-          .then((data: any) => {
-            let result = JSON.parse(data.responseText);
-            if (result.ticket) {
-              recentTicketId = result.ticket.id;
-              console.log('ticket created' + result.ticket);
-              client.invoke('routeTo', 'ticket', recentTicketId);
-            }
-          })
-          .catch((error: any) => {
-            console.error('ticket error', error);
-          });
-      };
-    },
-  );
+  window.addEventListener('messageerror', ev => {
+    log('message error', ev);
+  });
 });
 
 const formatRecordName = (name: string, type: string) => `[${type}] ${name}`;
@@ -303,3 +87,7 @@ const modal = () => {
     },
   }).then(() => console.log('Modal loaded!'));
 };
+
+interface VoiceDialoutEvent {
+  number: string;
+}
