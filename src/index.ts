6253833f9c308a9aa6cb0d89a5f863a8bcf26fd4
Esi're <esiri92@gmail.com>
2023-07-19 00:06:51 +0000

    chore: allow configuration of widget url on app installation.

--- src/index.ts
+++ src/index.ts
@@ -13,25 +13,6 @@ const client = ZAFClient.init();
 client.on('app.registered', (data: any) => {
   // console.log('app.registered', { data });
 
-  let host: string, port: string, url: string;
-
-  const form = document.getElementById('widget-placeholder')! as HTMLFormElement;
-  const hostEl = form.elements.namedItem('host')! as HTMLInputElement;
-  const portEl = form.elements.namedItem('port')! as HTMLInputElement;
-
-  hostEl.addEventListener('change', () => host = hostEl.value);
-  portEl.addEventListener('change', () => port = portEl.value);
-
-  form.addEventListener('submit', e => {
-    e.preventDefault();
-
-    if (!host || !port) return;
-
-    url = `https://${host}:${port}/pbx/etc/widget/zendesk/index.html`;
-
-    (document.getElementById('widget-container') as HTMLIFrameElement).src = url;
-  });
-
   let widgetWindow: Window;
 
   const messageName = (name: Events) => `brekeke:${name}`;
